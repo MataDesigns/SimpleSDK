8f147f49cd003d99e7a9b7b7f4edd82050628ec9
Nicholas Mata <nicholas@matadesigns.net>
2018-03-15 17:34:12 -0700

    Initial Commit

---
+++ SimpleSDK/SimpleSDK.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSDK.h
+//  SimpleSDK
+//
+//  Created by Nicholas Mata on 3/15/18.
+//  Copyright © 2018 MataDesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleSDK.
+FOUNDATION_EXPORT double SimpleSDKVersionNumber;
+
+//! Project version string for SimpleSDK.
+FOUNDATION_EXPORT const unsigned char SimpleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSDK/PublicHeader.h>
+
+
